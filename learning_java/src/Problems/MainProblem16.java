f7610168c8b8d3624249ed1c3bea9fc2bb70ad09
Gabriel <prismaticnetwork12@gmail.com>
2022-09-12 20:27:28 -0300

    feat: adding upcasting and downcasting

---
+++ learning_java/src/Problems/MainProblem16.java
@@ -0,0 +1,25 @@
+package Problems;
+
+import enums.OrderEnum;
+import models.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class MainProblem16 {
+  public static void main(String[] args) throws ParseException {
+    //Upcasting and Down casting with objects
+    //polimorfism
+    Account account = new Account();
+    Account savingsAccToAcc = new SavingsAccount();
+    Account businessAccountToAccount = new BusinessAccount();
+
+    //down casting
+    //in order to use methods that not contain in class itself you can use down casting or upcasting
+    BusinessAccount bcAccount = (BusinessAccount) new Account();
+
+    //instanceof to check if an object is an instance of something
+  }
+}
---
+++ learning_java/src/models/SavingsAccount.java
@@ -0,0 +1,26 @@
+package models;
+
+public class SavingsAccount extends Account{
+  private Double interestRate;
+  private Double balance;
+
+  public SavingsAccount() {
+  }
+
+  public SavingsAccount(int number, String holder, Double balance, Double interestRate) {
+    super(number, holder, balance);
+    this.interestRate = interestRate;
+  }
+
+  public Double getInterestRate() {
+    return interestRate;
+  }
+
+  public void setInterestRate(Double interestRate) {
+    this.interestRate = interestRate;
+  }
+
+  public void updateBalance() {
+    this.balance += 10;
+  }
+}
