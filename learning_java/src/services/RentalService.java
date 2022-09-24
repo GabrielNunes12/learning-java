45063658c896c6aeb784ca16ff650c70f88e952f
Gabriel <prismaticnetwork12@gmail.com>
2022-09-24 19:55:32 -0300

    feat: implementing interface TaxService

--- learning_java/src/services/RentalService.java
+++ learning_java/src/services/RentalService.java
@@ -6,10 +6,9 @@ import Model.Invoice;
 public class RentalService {
   private Double pricePerHour;
   private Double pricePerDay;
+  private TaxService brazilTaxService;
 
-  private BrazilTaxService brazilTaxService;
-
-  public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService brazilTaxService) {
+  public RentalService(Double pricePerHour, Double pricePerDay, TaxService brazilTaxService) {
     this.pricePerHour = pricePerHour;
     this.pricePerDay = pricePerDay;
     this.brazilTaxService = brazilTaxService;
